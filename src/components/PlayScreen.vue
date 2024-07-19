6b0e35d80b575ac0fb0538171f2ce41dcb9065ec
Bagration-34 <bagration-34@yandex.ru>
2024-07-19 16:27:01 +0300

    play

--- src/components/PlayScreen.vue
+++ src/components/PlayScreen.vue
@@ -10,6 +10,15 @@ export default {
 </script>
 
 <template>
+    <div id="container">
+        <div id="top">
+            <button class="btn-mini" @click="game.changeScreen(-1)">
+                <svg width="35" height="50" viewBox="4 0 50 50" fill="161618" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M35 50L11 25L35 0L40 4L20 25L40 45L35 50Z" />
+                </svg>
+            </button>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
