103ec123aa2c8740bc01e0fd0ab7e6a7d2ce31f4
Bagration-34 <bagration-34@yandex.ru>
2024-08-30 13:26:13 +0300

    save

--- src/scripts/gameLoop.js
+++ src/scripts/gameLoop.js
@@ -1,59 +1,60 @@
 export let requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
+    return (
+        window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
         function (callback) {
-            window.setTimeout(callback, 1000 / 20);
-        };
-})();
+            window.setTimeout(callback, 1000 / 20)
+        }
+    )
+})()
 
-import Stats from "stats.js";
+import Stats from 'stats.js'
 
-export default class GameLoop 
-{
-    constructor(update, render)
-    {
-        this.ms_per_update = 17;    // Интервал между вычислениями
-        this.elapsed = 0;            // Счетчик времени между кадрами
-        this.currentTime = 0;
-        this.pervious = Date.now();
-        this.lag = 0.0;
-        this.update = update;
-        this.render = render;
+export default class GameLoop {
+    constructor(update, render) {
+        this.ms_per_update = 17 // Интервал между вычислениями
+        this.elapsed = 0 // Счетчик времени между кадрами
+        this.currentTime = 0
+        this.pervious = Date.now()
+        this.lag = 0.0
+        this.update = update
+        this.render = render
 
-        this.stats = new Stats();
+        this.stats = new Stats()
         this.stats.showPanel(0)
+        this.stats.dom.style.width = '100%'
+        this.stats.dom.style.display = 'flex'
+        this.stats.dom.style.justifyContent = 'end'
         document.body.appendChild(this.stats.dom)
 
-        this.gameLoop();
+        this.gameLoop()
     }
-    
 
     gameLoop() {
-
         // Текущее вермя
         this.stats.begin()
-        this.currentTime = Date.now();
-        this.elapsed = this.currentTime - this.pervious; // Время между предыдущим и текущим кадром
-        this.pervious = this.currentTime;             // Сохраняем время текущего кадра
-        this.lag += this.elapsed;                     // Суммированное время между кадрами
+        this.currentTime = Date.now()
+        this.elapsed = this.currentTime - this.pervious // Время между предыдущим и текущим кадром
+        this.pervious = this.currentTime // Сохраняем время текущего кадра
+        this.lag += this.elapsed // Суммированное время между кадрами
 
-        this.update(this.lag);
-        this.lag -= this.elapsed;
+        this.update(this.lag)
+        this.lag -= this.elapsed
         /*
         // При накоплении лагов, змейка начнёт отставать на несколько итераций т.е перемещений
         // с помощью этого цикла мы нагоняем змейку к её нужному положению
         */
         while (this.lag >= this.ms_per_update) {
-            this.update(this.lag);
-            this.lag -= this.ms_per_update;
+            this.update(this.lag)
+            this.lag -= this.ms_per_update
         }
 
         // Рендерим кадр с нужны интервалом (this.ms_per_update)
-        this.render();
+        this.render()
         this.stats.end()
-        requestAnimFrame(this.gameLoop.bind(this));
+        requestAnimFrame(this.gameLoop.bind(this))
     }
 }
